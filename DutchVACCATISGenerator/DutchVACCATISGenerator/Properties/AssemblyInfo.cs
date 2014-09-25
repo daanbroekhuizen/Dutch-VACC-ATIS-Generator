12bc5a6e498e9ccca90a20f4cb9932a51f509f85
daanbroekhuizen <daan@familie-broekhuizen.nl>
2014-09-25 23:58:31 +0200

    * Version 1.0.0.6 in progress.

--- DutchVACCATISGenerator/DutchVACCATISGenerator/Properties/AssemblyInfo.cs
+++ DutchVACCATISGenerator/DutchVACCATISGenerator/Properties/AssemblyInfo.cs
@@ -34,6 +34,6 @@ using System.Resources;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 //[assembly: AssemblyFileVersion("0.4.0.0")]
-[assembly: AssemblyVersion("1.0.0.4")]
+[assembly: AssemblyVersion("1.0.0.6")]
 [assembly: NeutralResourcesLanguageAttribute("en")]
-[assembly: AssemblyFileVersionAttribute("1.0.0.4")]
+[assembly: AssemblyFileVersionAttribute("1.0.0.6")]
