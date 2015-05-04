118cfb1e842d0bcb05084a826d9134e93e9e5f43
Daan Broekhuizen <daan@familie-broekhuizen.nl>
2015-05-04 17:41:42 +0200

    - Removed old ATC Operational Information manual screenshots. * Updated ATC Operational Information manual for new v2 version. * Adds manual for new v2 release.

--- DutchVACCATISGenerator/DutchVACCATISGenerator/Properties/AssemblyInfo.cs
+++ DutchVACCATISGenerator/DutchVACCATISGenerator/Properties/AssemblyInfo.cs
@@ -34,6 +34,6 @@ using System.Resources;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 //[assembly: AssemblyFileVersion("0.4.0.0")]
-[assembly: AssemblyVersion("1.0.1.1")]
+[assembly: AssemblyVersion("2.0.0.0")]
 [assembly: NeutralResourcesLanguageAttribute("en")]
-[assembly: AssemblyFileVersionAttribute("1.0.1.1")]
+[assembly: AssemblyFileVersionAttribute("2.0.0.0")]
