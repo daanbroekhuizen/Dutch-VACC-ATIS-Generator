55334ebc82294241fcb91d819aecb8d0f8549f7f
Daan Broekhuizen <daan@familie-broekhuizen.nl>
2018-03-30 10:40:40 +0200

    Auto update logic.

---
+++ DutchVACCATISGenerator/DutchVACCATISGenerator.Test/Logic/AutoUpdateTests.cs
@@ -0,0 +1,28 @@
+﻿using DutchVACCATISGenerator.Logic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DutchVACCATISGenerator.Test.Logic
+{
+    [TestClass]
+    public class AutoUpdateTests
+    {
+        private readonly IAutoUpdateLogic autoUpdateLogic;
+
+        public AutoUpdateTests()
+        {
+            autoUpdateLogic = new AutoUpdateLogic();
+        }
+
+        [TestMethod]
+        public void AutoUpdateTest()
+        {
+            //Act
+            autoUpdateLogic.AutoUpdate().Wait();
+        }
+    }
+}
---
+++ DutchVACCATISGenerator/DutchVACCATISGenerator/Logic/AutoUpdateLogic.cs
@@ -0,0 +1,101 @@
+﻿using DutchVACCATISGenerator.Types.Application;
+using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DutchVACCATISGenerator.Logic
+{
+    public interface IAutoUpdateLogic
+    {
+        /// <summary>
+        /// Download latest version of Dutch VACC ATIS Generator.
+        /// </summary>
+        Task AutoUpdate();
+    }
+
+    public class AutoUpdateLogic : IAutoUpdateLogic
+    {
+        private string executablePath;
+        private string zipName;
+
+        public async Task AutoUpdate()
+        {
+            //Set path of executable.
+            executablePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"\";
+
+            //Start downloading latest version.
+            await DownloadLatestVersion();
+        }
+
+        private async Task DownloadLatestVersion()
+        {
+            try
+            {
+                var webClient = new WebClient();
+
+                webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(DownloadFileCompleted);
+                webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(ApplicationEvents.DownloadProgressChanged);
+
+                zipName = GetZipName();
+
+                //Download the zip file.
+                await webClient.DownloadFileTaskAsync(new Uri("http://daanbroekhuizen.com/Dutch VACC/Dutch VACC ATIS Generator/" + zipName), executablePath + zipName);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        private string GetZipName()
+        {
+            //Request zip file name.
+#if DEBUG
+            var request = WebRequest.Create("http://daanbroekhuizen.com/Dutch VACC/Dutch VACC ATIS Generator/Version/filename2.php");
+#else
+            var request = WebRequest.Create("http://daanbroekhuizen.com/Dutch VACC/Dutch VACC ATIS Generator/Version/filename.php");
+#endif
+            var response = request.GetResponse();
+
+            //Read zip file name.
+            return new StreamReader(response.GetResponseStream()).ReadToEnd().Trim();
+        }
+
+        private void DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            try
+            {
+                //Delete temp folder if exists.
+                if (Directory.Exists($@"{Path.GetDirectoryName(executablePath)}\temp"))
+                    Directory.Delete($@"{executablePath}\temp", true);
+
+                //Extract zip.
+                ZipFile.ExtractToDirectory(executablePath + zipName, executablePath + @"\temp");
+
+                //Set temp folder to be hidden.
+                DirectoryInfo directoryInfo = Directory.CreateDirectory(executablePath + @"\temp");
+                directoryInfo.Attributes = FileAttributes.Directory | FileAttributes.Hidden | FileAttributes.ReadOnly;
+
+                //Delete zip.
+                File.Delete(executablePath + zipName);
+
+                //Start setup.
+                Process.Start(executablePath + @"\temp\" + "Dutch VACC ATIS Generator - Setup.exe");
+
+                //Exit program to run setup.
+                Application.Exit();
+            }
+            catch
+            {
+                //TODO What happens here?
+                throw;
+            }
+        }
+    }
+}
