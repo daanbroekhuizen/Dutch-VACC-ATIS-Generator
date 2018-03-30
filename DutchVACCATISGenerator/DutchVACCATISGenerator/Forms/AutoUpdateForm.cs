e25ca65b9805451548b16a622142119ecb26620b
Daan Broekhuizen <daan@familie-broekhuizen.nl>
2018-03-30 10:37:36 +0200

    Moved auto update form.

---
+++ DutchVACCATISGenerator/DutchVACCATISGenerator/Forms/AutoUpdateForm.cs
@@ -0,0 +1,36 @@
+﻿using DutchVACCATISGenerator.Logic;
+using DutchVACCATISGenerator.Types.Application;
+using System;
+using System.Net;
+using System.Windows.Forms;
+
+namespace DutchVACCATISGenerator
+{
+    public partial class AutoUpdaterForm : Form
+    {
+        public AutoUpdaterForm(IAutoUpdateLogic autoUpdateLogic)
+        {
+            InitializeComponent();
+
+            ApplicationEvents.DownloadProgressChangedEvent += DownloadProgressChanged;
+
+            try
+            {
+                autoUpdateLogic.AutoUpdate();
+            }
+            catch (Exception)
+            {
+                this.Close();
+            }
+        }
+
+        private void DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            if (progressBar.InvokeRequired)
+                progressBar.Invoke(new Action(() => progressBar.Value = e.ProgressPercentage));
+            else
+                progressBar.Value = e.ProgressPercentage;
+        }
+    }
+}
+
