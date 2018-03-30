c2799ae36a8ba53f638c53b7ec549c517ba72773
Daan Broekhuizen <daan@familie-broekhuizen.nl>
2018-03-30 10:48:35 +0200

    Method name.

--- DutchVACCATISGenerator/DutchVACCATISGenerator/Program.cs
+++ DutchVACCATISGenerator/DutchVACCATISGenerator/Program.cs
@@ -19,12 +19,12 @@ namespace DutchVACCATISGenerator
             //Bootstrap SimpleInjector.
             container = container.Bootstrap();
 
-            Application.ThreadException += new ThreadExceptionEventHandler(MyCommonExceptionHandlingMethod);
+            Application.ThreadException += new ThreadExceptionEventHandler(ThreadExceptionEventHandler);
 
             Application.Run(container.GetInstance<DutchVACCATISGenerator>());
         }
 
-        private static void MyCommonExceptionHandlingMethod(object sender, ThreadExceptionEventArgs t)
+        private static void ThreadExceptionEventHandler(object sender, ThreadExceptionEventArgs t)
         {
             MessageBox.Show("An unexpected error has occurred. The application will now exit.", "Error");
 
