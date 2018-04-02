5fed290c702fc6bea2981f495ddea0c9dbe63e3d
Daan Broekhuizen <daan@familie-broekhuizen.nl>
2018-04-02 22:25:16 +0200

    SplitBy extension.

--- DutchVACCATISGenerator/DutchVACCATISGenerator/Extensions/StringExtensions.cs
+++ DutchVACCATISGenerator/DutchVACCATISGenerator/Extensions/StringExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace DutchVACCATISGenerator.Extensions
 {
@@ -7,8 +8,8 @@ namespace DutchVACCATISGenerator.Extensions
         /// <summary>
         /// Check if input string is numbers only.
         /// </summary>
-        /// <param name="input">String to check.</param>
-        /// <returns>Boolean indicating if the string is numbers only.</returns>
+        /// <param name="input">String to check</param>
+        /// <returns>Boolean indicating if the string is numbers only</returns>
         public static bool IsNumbersOnly(this string input)
         {
             return input.All(char.IsDigit);
@@ -17,12 +18,31 @@ namespace DutchVACCATISGenerator.Extensions
         /// <summary>
         /// Check if input string is a certain size.
         /// </summary>
-        /// /// <param name="input">String to check.</param>
-        /// <param name="lenght">Size to check to.</param>
-        /// <returns>Boolean indicating if the string is the size inputted.</returns>
+        /// /// <param name="input">String to check</param>
+        /// <param name="lenght">Size to check to</param>
+        /// <returns>Boolean indicating if the string is the size inputted</returns>
         public static bool IsLength(this string input, int lenght)
         {
             return (input.Length == lenght);
+        }
+
+        /// <summary>
+        /// Splits a string by the given word.
+        /// </summary>
+        /// <param name="input">String to split</param>
+        /// <param name="splitBy">Word to split by</param>
+        /// <returns>Split string as array</returns>
+        public static string[] SplitBy(this string input, string splitBy)
+        {
+            var regex = new Regex($@"\b{splitBy}\b");
+
+            var split = regex.Split(input);
+
+            //Trim every entry.
+            for (var i = 0; i < split.Length; i++)
+                split[i] = split[i].Trim();
+
+            return split;
         }
     }
 }
