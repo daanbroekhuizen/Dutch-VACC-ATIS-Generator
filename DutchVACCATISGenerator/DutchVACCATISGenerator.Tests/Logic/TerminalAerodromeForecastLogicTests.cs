16aec25d5c4638df887959cbc3dc1781621d5648
Daan Broekhuizen <daan@familie-broekhuizen.nl>
2018-04-14 09:07:56 +0200

    TAF tests.

--- DutchVACCATISGenerator/DutchVACCATISGenerator.Tests/Logic/TerminalAerodromeForecastLogicTests.cs
+++ DutchVACCATISGenerator/DutchVACCATISGenerator.Tests/Logic/TerminalAerodromeForecastLogicTests.cs
@@ -41,5 +41,35 @@ namespace DutchVACCATISGenerator.Test.Logic
             Assert.AreNotEqual(terminalAerodromeForecast, string.Empty);
             Assert.IsTrue(terminalAerodromeForecast.Contains(applicationVariables.SelectedAirport));
         }
+
+        [TestMethod]
+        public void GetTerminalAerodromeForecast_WithCOR_ReturnsCORTerminalAerodromeForecast()
+        {
+            //Arrange
+            applicationVariables.SelectedAirport = "EHAM";
+            var terminalAerodromeForecastRaw = "TAF COR EHAM 140455Z 1406/1512 25007KT 5000 BR BKN020 BECMG 1406/1408 9999 NSW SCT030 BECMG 1414/1417 34007KT CAVOK BECMG 1419/1422 05005KT PROB30 TEMPO 1422/1504 6000 RA -SHRA SCT040CB BECMG 1500/1503 12005KT 5000 BR PROB30 TEMPO 1503/1506 2000 BECMG 1506/1508 18007KT 9999 NSW SCT025=";
+
+            //Act
+            var terminalAerodromeForecast = terminalAerodromeForecastLogic.GetTerminalAerodromeForecast(terminalAerodromeForecastRaw);
+
+            //Assert
+            Assert.AreNotEqual(terminalAerodromeForecast, string.Empty);
+            Assert.IsTrue(terminalAerodromeForecast.Contains($"TAF COR {applicationVariables.SelectedAirport}"));
+        }
+
+        [TestMethod]
+        public void GetTerminalAerodromeForecast_WithAMD_ReturnsCORTerminalAerodromeForecast()
+        {
+            //Arrange
+            applicationVariables.SelectedAirport = "EHAM";
+            var terminalAerodromeForecastRaw = "TAF AMD EHAM 140455Z 1406/1512 25007KT 5000 BR BKN020 BECMG 1406/1408 9999 NSW SCT030 BECMG 1414/1417 34007KT CAVOK BECMG 1419/1422 05005KT PROB30 TEMPO 1422/1504 6000 RA -SHRA SCT040CB BECMG 1500/1503 12005KT 5000 BR PROB30 TEMPO 1503/1506 2000 BECMG 1506/1508 18007KT 9999 NSW SCT025=";
+
+            //Act
+            var terminalAerodromeForecast = terminalAerodromeForecastLogic.GetTerminalAerodromeForecast(terminalAerodromeForecastRaw);
+
+            //Assert
+            Assert.AreNotEqual(terminalAerodromeForecast, string.Empty);
+            Assert.IsTrue(terminalAerodromeForecast.Contains($"TAF AMD {applicationVariables.SelectedAirport}"));
+        }
     }
 }
